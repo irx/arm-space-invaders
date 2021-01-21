e58b44a04ba800040eb1bf9a9a926d4581b13559
Maksymilian Mruszczak <u@1ux.org>
2021-01-21 22:10:05 +0100

    Implement entity management system

---
+++ entity.c
@@ -0,0 +1,74 @@
+/*
+ * 2021, Maksymilian Mruszczak <u at one u x dot o r g>
+ *
+ * Entities procedures
+ */
+
+
+#include "MKL05Z4.h"
+#include "sprite.h"
+#include "entity.h"
+
+
+#define ENTITY_CNT		64
+
+
+static Entity			entity_buf[ENTITY_CNT];
+static Entity			*entity_first;
+static Entity			*entity_last;
+
+
+void
+init_entities(void)
+{
+	int i;
+	for (i = 0; i < ENTITY_CNT; ++i)
+		entity_buf[i].exists = 0;
+	entity_first = NULL;
+	entity_last = NULL;
+}
+
+Entity *
+create_entity(const Sprite *spr, const Sprite *alt, uint8_t x, uint8_t y)
+{
+	int i = -1; /* search for free space */
+	while (entity_buf[++i].exists)
+		if (i+1 >= ENTITY_CNT)
+			return NULL;
+
+	entity_buf[i].exists = 1;
+	entity_buf[i].frame = 0;
+	entity_buf[i].sprite[0] = (Sprite *)spr;
+	entity_buf[i].sprite[1] = (Sprite *)alt;
+	if (entity_last == NULL) {
+		entity_buf[i].prev = NULL;
+		entity_first = &entity_buf[i];
+	} else
+		entity_buf[i].prev = entity_last;
+	entity_buf[i].next = NULL;
+	entity_last = &entity_buf[i];
+	return entity_last;
+}
+
+void
+delete_entity(Entity *e)
+{
+	/* relink list and freeup the slot */
+	if (e->prev != NULL)
+		e->prev->next = e->next;
+	if (e->next != NULL)
+		e->next->prev = e->prev;
+	if (e == entity_first)
+		entity_first = e->next;
+	if (e == entity_last)
+		entity_last = e->prev;
+	e->exists = 0;
+}
+
+void
+render_entities(void)
+{
+	Entity *e = entity_first;
+	while (e != NULL)
+		draw_sprite(e->sprite[e->frame], e->x, e->y);
+}
---
+++ entity.h
@@ -0,0 +1,18 @@
+/*
+ * 2021, Maksymilian Mruszczak <u at one u x dot o r g>
+ *
+ * Entities procedures
+ */
+
+
+typedef struct Entity Entity;
+struct Entity {
+	Sprite *sprite[2];
+	uint8_t exists, frame, x, y;
+	Entity *next, *prev;
+};
+
+void			init_entities(void);
+Entity			*create_entity(const Sprite *, const Sprite *, uint8_t, uint8_t);
+void			delete_entity(Entity *);
+void			render_entities(void);
