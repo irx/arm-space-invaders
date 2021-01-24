c72e08648d5ed65f5ee683bef072f3c7aefbc1de
Maksymilian Mruszczak <u@1ux.org>
2021-01-24 02:21:52 +0100

    Reorganise main and wait for keypress before start

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-/*
+/**
  * 2021, Maksymilian Mruszczak <u at one u x dot o r g>
  *
- * Waveshare SSD1331 OLED display test for FRDM-KL05Z
- * based on example for STM32 XNUCLEO-F103RB from Waveshare Team
+ * Space Invaders implementation for FRDM-KL05Z
+ * used with Waveshare SSD1331 OLED display
+ * and UART at 9600 for user input
  */
 
 #include "MKL05Z4.h"
@@ -27,48 +28,14 @@ main(void)
 	init_sprites();
 	init_entities();
 
-	ssd1331_clear_screen(RED);
-	delay_ms(1000);
-	delay_ms(1000);
-	ssd1331_clear_screen(GREEN);
-	delay_ms(1000);
-	delay_ms(1000);
-	ssd1331_clear_screen(BLUE);
-	delay_ms(1000);
-	delay_ms(1000);
+	draw_sprite(&sprite_logo, 0, 0);
+	delay_ms(2000);
 	ssd1331_clear_screen(BLACK);
-	
-	ssd1331_display_string(10, 0, "0.95inch OLED", FONT_1206, WHITE);
-	ssd1331_display_string(0, 16, "This is a demo for SSD1331 OLED board!", FONT_1206, YELLOW);
-	ssd1331_display_string(22, 52, "WaveShare", FONT_1206, GREEN);
-	delay_ms(1000);
-	delay_ms(1000);
-	ssd1331_clear_screen(BLACK);
+	ssd1331_display_string(9, 0, "Space", FONT_1608, GREEN);
+	ssd1331_display_string(32, 16, "Invaders", FONT_1608, GREEN);
+	ssd1331_display_string(10, 52, "Press any key", FONT_1206, WHITE);
 
-	ssd1331_draw_bitmap(0, 2, &c_chSingal816[0], 16, 8, YELLOW);
-	ssd1331_draw_bitmap(19, 2, &c_chMsg816[0], 16, 8, YELLOW);
-	ssd1331_draw_bitmap(38, 2, &c_chBluetooth88[0], 8, 8, YELLOW);
-	ssd1331_draw_bitmap(52, 2, &c_chGPRS88[0], 8, 8, YELLOW);
-	ssd1331_draw_bitmap(66, 2, &c_chAlarm88[0], 8, 8, YELLOW);
-	ssd1331_draw_bitmap(80, 2, &c_chBat816[0], 16, 8, YELLOW);
-
-	ssd1331_draw_3216char(8,16, '2', BLUE);
-	ssd1331_draw_3216char(24,16, '3', BLUE);
-	ssd1331_draw_3216char(40,16, ':', RED);
-	ssd1331_draw_3216char(56,16, '5', BLUE);
-	ssd1331_draw_3216char(72,16, '6', BLUE);
-
-	ssd1331_display_string(9, 48, "2015-01-27", FONT_1608, GREEN);
-
-	logo = create_entity(&sprite_logo, &sprite_logo, 0, 0, 0);
-	invader = create_entity(&sprite_invader, &sprite_invader_alt, 10, 10, 0);
-
-	for (;;) {
-		render_entities();
-		delay_ms(2000);
-		++(invader->y);
-		invader->frame = invader->y % 2;
-	}
+	game_loop();
 }
 
 void
